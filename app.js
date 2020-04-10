fa5bb62af2e7a510c2767b00ea8ef2f1f776264e
Alejandro Chum <Alejo@maybellines-mbp.lan>
2020-04-10 09:31:55 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+var path = require('path');
+var root = __dirname;
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+// app.use(express.json());
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res){
+    res.render("logIn");
+});
+
+app.post("/horario", function(req, res){
+  res.render("horario-v2");
+});
+
+app.get("/clase", function(req, res){
+  res.render("clase");
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server started on port 3000");
+});
---
+++ public/scripts/horario.js
@@ -0,0 +1,170 @@
+//jshint esversion:6
+$("#lengua").click(function(){
+  $("#sidebar").html(`<div class="row">
+            <div class="col-11">
+              <div class="sidebar-title">
+                <h1 id="sidebar-title">Lengua</h1>
+              </div>
+              <div class="sidebar-content">
+
+                <div class="card mx-4">
+                  <div class="card-body">
+                    <h5>PROFESOR</h5>
+                    <p><i class="fas fa-user fa-lg"></i><span class="ml-3">Roberto Sanchez</span></p>
+                    <i class="fas fa-envelope fa-sm"></i><span class="ml-3">robert.sanchez@gmail.com</span>
+                  </div>
+                </div>
+
+                <div class="card mx-4 mt-3">
+                  <div class="card-body">
+                    <h5>PENDIENTE</h5>
+                    <br>
+                    <h6 class="card-subtitle mb-2 text-muted">Questionario #5</h6>
+                    <p class="card-text">Completa el cuestionario antes de Abril 20</p>
+                    <a href="#" class="card-link">Ver Cuestionario</a>
+                  </div>
+                </div>
+
+                <div class="mt-5 text-right">
+                  <a href="#">Ir a la clase</a>
+                </div>
+              </div>
+            </div>
+          </div>`);
+});
+
+$("#matematica").click(function(){
+  $("#sidebar").html(`<div class="row">
+            <div class="col-11">
+              <div class="sidebar-title">
+                <h1 id="sidebar-title">Matematica</h1>
+              </div>
+              <div class="sidebar-content">
+
+                <div class="card mx-4">
+                  <div class="card-body">
+                    <h5>PROFESOR</h5>
+                    <p><i class="fas fa-user fa-lg"></i><span class="ml-3">Roberto Sanchez</span></p>
+                    <i class="fas fa-envelope fa-sm"></i><span class="ml-3">robert.sanchez@gmail.com</span>
+                  </div>
+                </div>
+
+                <div class="card mx-4 mt-3">
+                  <div class="card-body">
+                    <h5>PENDIENTE</h5>
+                    <br>
+                    <h6 class="card-subtitle mb-2 text-muted">Questionario #5</h6>
+                    <p class="card-text">Completa el cuestionario antes de Abril 20</p>
+                    <a href="#" class="card-link">Ver Cuestionario</a>
+                  </div>
+                </div>
+
+                <div class="mt-5 text-right">
+                  <a href="/clase">Ir a la clase</a>
+                </div>
+              </div>
+            </div>
+          </div>`);
+});
+
+$("#literatura").click(function(){
+  $("#sidebar").html(`<div class="row">
+            <div class="col-11">
+              <div class="sidebar-title">
+                <h1 id="sidebar-title">Literatura</h1>
+              </div>
+              <div class="sidebar-content">
+
+                <div class="card mx-4">
+                  <div class="card-body">
+                    <h5>PROFESOR</h5>
+                    <p><i class="fas fa-user fa-lg"></i><span class="ml-3">Roberto Sanchez</span></p>
+                    <i class="fas fa-envelope fa-sm"></i><span class="ml-3">robert.sanchez@gmail.com</span>
+                  </div>
+                </div>
+
+                <div class="card mx-4 mt-3">
+                  <div class="card-body">
+                    <h5>PENDIENTE</h5>
+                    <br>
+                    <h6 class="card-subtitle mb-2 text-muted">Questionario #5</h6>
+                    <p class="card-text">Completa el cuestionario antes de Abril 20</p>
+                    <a href="#" class="card-link">Ver Cuestionario</a>
+                  </div>
+                </div>
+
+                <div class="mt-5 text-right">
+                  <a href="#">Ir a la clase</a>
+                </div>
+              </div>
+            </div>
+          </div>`);
+});
+
+$("#historia").click(function(){
+  $("#sidebar").html(`<div class="row">
+            <div class="col-11">
+              <div class="sidebar-title">
+                <h1 id="sidebar-title">Historia</h1>
+              </div>
+              <div class="sidebar-content">
+
+                <div class="card mx-4">
+                  <div class="card-body">
+                    <h5>PROFESOR</h5>
+                    <p><i class="fas fa-user fa-lg"></i><span class="ml-3">Roberto Sanchez</span></p>
+                    <i class="fas fa-envelope fa-sm"></i><span class="ml-3">robert.sanchez@gmail.com</span>
+                  </div>
+                </div>
+
+                <div class="card mx-4 mt-3">
+                  <div class="card-body">
+                    <h5>PENDIENTE</h5>
+                    <br>
+                    <h6 class="card-subtitle mb-2 text-muted">Questionario #5</h6>
+                    <p class="card-text">Completa el cuestionario antes de Abril 20</p>
+                    <a href="#" class="card-link">Ver Cuestionario</a>
+                  </div>
+                </div>
+
+                <div class="mt-5 text-right">
+                  <a href="#">Ir a la clase</a>
+                </div>
+              </div>
+            </div>
+          </div>`);
+});
+
+$("#AP").click(function(){
+  $("#sidebar").html(`<div class="row">
+            <div class="col-11">
+              <div class="sidebar-title">
+                <h1 id="sidebar-title">A.P</h1>
+              </div>
+              <div class="sidebar-content">
+
+                <div class="card mx-4">
+                  <div class="card-body">
+                    <h5>PROFESOR</h5>
+                    <p><i class="fas fa-user fa-lg"></i><span class="ml-3">Roberto Sanchez</span></p>
+                    <i class="fas fa-envelope fa-sm"></i><span class="ml-3">robert.sanchez@gmail.com</span>
+                  </div>
+                </div>
+
+                <div class="card mx-4 mt-3">
+                  <div class="card-body">
+                    <h5>PENDIENTE</h5>
+                    <br>
+                    <h6 class="card-subtitle mb-2 text-muted">Questionario #5</h6>
+                    <p class="card-text">Completa el cuestionario antes de Abril 20</p>
+                    <a href="#" class="card-link">Ver Cuestionario</a>
+                  </div>
+                </div>
+
+                <div class="mt-5 text-right">
+                  <a href="#">Ir a la clase</a>
+                </div>
+              </div>
+            </div>
+          </div>`);
+});
